ba1cc57c2eb67b62a744036e99492a03458ab692
Thinh Vu <vutrongthinhk7@gmail.com>
2020-10-05 14:46:44 +0700

    Update canvaskit-api.js
    Change custome newLine Font size

--- src/canvaskit-api.js
+++ src/canvaskit-api.js
@@ -98,9 +98,9 @@ class CanvaskitApi {
     this.fontSize = fontSize;
   }
 
-  newLine() {
+  newLine(customNewLineFontSize) {
     const currentFontSize = this.fontSize;
-    this.fontSize = this.newLineFontSize;
+    this.fontSize = customNewLineFontSize || this.newLineFontSize;
     this.println('\n');
     this.fontSize = currentFontSize;
   }
