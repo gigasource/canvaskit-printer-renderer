a00b8e4c5231154b6ceee4cc05757411e9d7afc3
danglam105 <danglam105@gmail.com>
2020-10-28 14:36:39 +0700

    Add missing file from previous commit

---
+++ src/print-worker-thread/worker-script.js
@@ -0,0 +1,24 @@
+const wkPool = require('workerpool');
+const PureImagePrinter = require('../pure-image-printer');
+let printer;
+
+async function execPrintTasks(printTasks) {
+  if (!printer) printer = new PureImagePrinter(560, 250, {noResizing: true});
+
+  if (printer.currentPrintY > 0) printer._reset();
+
+  for (let i = 0; i < printTasks.length; i++) {
+    const {fnName, argArray} = printTasks[i];
+    await printer[fnName].apply(printer, argArray);
+  }
+
+  return {
+    data: printer.canvas.data.slice(0, Math.ceil(printer.originalCanvasWidth * printer.currentPrintY / 8)),
+    width: printer.originalCanvasWidth,
+    height: printer.currentPrintY
+  };
+}
+
+wkPool.worker({
+  execPrintTasks,
+});
