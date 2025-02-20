b84fdac275e01e14365cc1c61de49e539f1329fb
ThinhVu <thinhvux@protonmail.com>
2025-02-21 01:53:13 +0700

    v0.0.82 - fix font

--- scripts/pkg.config.js
+++ scripts/pkg.config.js
@@ -5,7 +5,7 @@ pkgConfig.assets.push('../assets/fonts/Verdana.ttf');
 pkgConfig.assets.push('../assets/fonts/Verdana_Bold.ttf');
 pkgConfig.assets.push('../assets/fonts/Verdana_Italic.ttf');
 pkgConfig.assets.push('../assets/fonts/Verdana_Bold_Italic.ttf');
-pkgConfig.assets.push('../assets/fonts/NotoSansSCVF.ttf');
+pkgConfig.assets.push('../assets/fonts/NotoSansSC-Regular.ttf');
 module.exports = pkgConfig;
 
 
