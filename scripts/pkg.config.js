e23309d66f586f29f8ecb41157c28ab3c9d9a78a
ThinhVu <thinhvux@protonmail.com>
2025-12-17 16:06:17 -0800

    remove NotoSansSC font assets

--- scripts/pkg.config.js
+++ scripts/pkg.config.js
@@ -5,7 +5,6 @@ pkgConfig.assets.push('../assets/fonts/Verdana.ttf');
 pkgConfig.assets.push('../assets/fonts/Verdana_Bold.ttf');
 pkgConfig.assets.push('../assets/fonts/Verdana_Italic.ttf');
 pkgConfig.assets.push('../assets/fonts/Verdana_Bold_Italic.ttf');
-pkgConfig.assets.push('../assets/fonts/NotoSansSC-Regular.ttf');
 pkgConfig.assets.push('../assets/fonts/Roboto_Condensed.ttf');
 pkgConfig.assets.push('../assets/fonts/Roboto_Condensed_Bold.ttf');
 pkgConfig.assets.push('../assets/fonts/Roboto_Condensed_Italic.ttf');
